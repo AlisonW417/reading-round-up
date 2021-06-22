b7bd8ed31db37abd7faba0357a013debcfd212f7
Alison Williams <apw5011@gmail.com>
2021-06-22 14:46:14 -0400

    added helpers

--- reading-round-up-backend/app/controllers/application_controller.rb
+++ reading-round-up-backend/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    helpers do 
+        def current_user
+            # mock version of logged in user
+            User.first
+        end 
+
+        def logged_in?
+            !!current_user
+        end 
+    end 
+
 end
