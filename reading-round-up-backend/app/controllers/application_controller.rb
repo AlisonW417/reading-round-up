b884804c8e78b4e61608a149e12242d59f0801bb
Alison Williams <apw5011@gmail.com>
2021-06-22 15:15:25 -0400

    added react frontend

--- reading-round-up-backend/app/controllers/application_controller.rb
+++ reading-round-up-backend/app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 class ApplicationController < ActionController::API
-    helpers do 
-        def current_user
-            # mock version of logged in user
-            User.first
-        end 
-
-        def logged_in?
-            !!current_user
-        end 
+    def current_user
+        # mock version of logged in user
+        User.first
     end 
 
+    def logged_in?
+        !!current_user
+    end 
 end
