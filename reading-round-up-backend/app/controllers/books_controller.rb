5a48a3d2a38d33f29b5081aa86d4cbce9a0987f6
Alison Williams <apw5011@gmail.com>
2021-06-24 15:23:36 -0400

    added books controller index

--- reading-round-up-backend/app/controllers/books_controller.rb
+++ reading-round-up-backend/app/controllers/books_controller.rb
@@ -1,2 +1,12 @@
 class BooksController < ApplicationController
+    def index 
+        if logged_in? 
+            @books = current.user.book
+            render json: @books 
+        else 
+            render json: {
+                message: "Log in to view books"
+            }
+        end
+    end 
 end
