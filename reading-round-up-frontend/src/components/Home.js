1d9f869c1f8fa9870254b391a493c86548a96dde
Alison Williams <apw5011@gmail.com>
2021-07-07 12:15:09 -0400

    update to home component

--- reading-round-up-frontend/src/components/Home.js
+++ reading-round-up-frontend/src/components/Home.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Container, Card } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 
 const Home = () => (
     <Container>
         <div>
-            <Card>
-                <h1>Welcome to Reading Round-up!</h1>
-                <h2>Please <Link to='/login'>Log in</Link> or <Link to='/signup'>Sign up</Link>.</h2>
-            </Card>
+            <h1>Welcome to Reading Round-up!</h1>
+            <h2>Please <Link to='/login'>Log in</Link> or <Link to='/signup'>Sign up</Link>.</h2>
         </div>
     </Container>
 )
